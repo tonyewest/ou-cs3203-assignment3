27094f9af82cfb8de2914c279f864a2518691e0b
tonyewest <tharcourt05@ou.edu>
2023-09-13 21:48:51 -0500

    Added main class with user input

--- assignment3.java
+++ assignment3.java
@@ -1,5 +1,7 @@
-public static int arraySum(int[] numbers) {
-    int sum = 0;
+import java.util.Scanner; // import the Scanner Class
+
+public static int arraySum(int[] numbers) { // Compute the sum of all the numbers in the array
+    int sum = 0; 
 
     for(int number : numbers) {
         sum += number;
@@ -8,7 +10,7 @@ public static int arraySum(int[] numbers) {
     return sum;
 }
 
-public static int arrayProduct(int[] numbers) {
+public static int arrayProduct(int[] numbers) { // Compute the product of the numbers in the array
     int product = 1;
     
     for(int number : numbers) {
@@ -18,9 +20,18 @@ public static int arrayProduct(int[] numbers) {
     return product;
 }
 
-public static void main(String[] args) {
-    int[] testNumbers = {1, 2, 3, 4, 5};
-    int test = arraySum(testNumbers);
+public static void main(String[] args) { // main method: collect user input for array numbers and print sum/product
+    Scanner stdin = new Scanner(System.in);
+    System.out.println("Hello! Enter your list of numbers:");
+    int[] numbers = new int[10];
 
-    System.out.println("The sum is: " + test);
+    numbers[0] = stdin.nextInt();
+    int currInt;
+    int count = 1;
+    while (currInt != -1 && count < 9) {
+        numbers[count] == stdin.nextInt();
+    }
+
+    System.out.println("The sum of the numbers is: " + arraySum);
+    System.out.println("The product of the numbers is: " + arrayProduct);
 }
